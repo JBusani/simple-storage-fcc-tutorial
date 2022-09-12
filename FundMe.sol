2d7d8c20d975f76d91309e30e069a6b0f29552e7
JPB <53477487+JBusani@users.noreply.github.com>
2022-09-12 14:50:15 +0900

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -15,18 +15,22 @@ contract FundMe{
 
         require(msg.value >= minimumUSD, "Didn't send enough");
         //but let's convert it
+        //msg.value is ethereum
+
     }
 
 
     function getPrice() public view returns(uint256){
         //abi - how? interface
         //Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-
-            //destructor by looking at github and on using data example on chainlink page too
+        //destructor by looking at github and on using data example on chainlink page too
+        
+        
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
         (,int256 price,,,) =  priceFeed.latestRoundData();         
         return uint256(price * 1e10); // 1 ** 10 == 1000000000
             
+            //return in USD
             //these commas were important and needed to match in number and placement
             //we can convert the int to uint easily like this. remember
             //solidity doesn't do decimal places
@@ -41,7 +45,19 @@ contract FundMe{
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
         return priceFeed.version();
     }
-    function getConversionRate() public {}
+
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+         uint256 ethPrice = getPrice();
+
+        //3000 $ -> 3000_000000000000000000 ETH / USD
+        //1_000000000000000000 ETH
+        //2000.9999999...
+        //so it is 3000 dollars
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+        //always multiple before dividing
 
     //function withdraw(){}
 }
